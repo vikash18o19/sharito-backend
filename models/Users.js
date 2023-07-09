55a9d1c086882c10c47a9617ff55b5a0bed550cf
Vikash Kumar <vikash0cv@gmail.com>
2023-07-09 15:26:48 +0530

    added users model

---
+++ models/Users.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  modifiedAt: {
+    type: Date,
+    default: null,
+  },
+  passwordResetToken: {
+    type: String,
+    default: null,
+  },
+  passwordResetExpires: {
+    type: Date,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
